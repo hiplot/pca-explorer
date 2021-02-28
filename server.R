30d13729f79ef419f1d253493d03bca7480d67e9
Jianfeng Li <lee_jianfeng@sjtu.edu.cn>
2021-02-28 22:29:56 +0800

    update server.R

--- server.R
+++ server.R
@@ -10,8 +10,8 @@ list.of.packages <- c("ggplot2",
                       "MASS",
                       "tabplot")
 
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+#new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+#if(length(new.packages)) install.packages(new.packages)
 
 # load all these
 lapply(list.of.packages, require, character.only = TRUE)
